837ffe77f76e33890a19339be44199a3c3fb5114
Ramires Gomes <ramires.gb@gmail.com>
2016-12-14 20:06:14 -0200

    Enviando projeto

---
+++ app/src/main/java/t2/ramires/org/trabalho/Greeting.java
@@ -0,0 +1,15 @@
+package t2.ramires.org.trabalho;
+
+public class Greeting
+{
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
